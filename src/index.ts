f9e2953b0f5471d5eeb4886376e91a31eebe4ee2
Gerasim Gerasimov <dialix@yandex.ru>
2023-05-25 23:35:41 +0700

    сохранись

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,7 @@ import { Settings } from "./utils/config";
 
 console.log('Start DUMB2CSV');
 
-//const src: Uint8Array = new Uint8Array(fs.readFileSync('./data/22222544.DMP'));
-const src: Uint8Array = new Uint8Array(fs.readFileSync('./data/22222330.DMP'));
+const src: Uint8Array = new Uint8Array(fs.readFileSync('./data/25192845.DMP'));
 
 class TDump{
   T1000Hz: number;//u16
@@ -26,7 +25,7 @@ const FileSize: number = src.byteLength;
 
 const step: number = 26;
 var index: number = 0;
-var res: string = 'T1000Hz;Igen;UgenAB;UgenBC;UgenCA;Iexc;Uexc;Trig;Faults;\n';
+var res: string = 'T1000Hz;Igen;UgenAB;UgenBC;UgenCA;Iexc;Uexc;Trig;Faults;DateTime\n';
 
 while (index < FileSize) {
   let s: string = '';
@@ -37,12 +36,13 @@ while (index < FileSize) {
   dump.UgenAB = d.getInt16(4, true);
   dump.UgenBC = d.getInt16(6, true);
   dump.UgenCA = d.getInt16(8, true);
-  dump.Iexc = d.getUint16(10, true);
-  dump.Uexc = d.getInt16(12, true);
+  dump.Iexc = d.getUint16(10, true) * 0.01;
+  dump.Uexc = d.getInt16(12, true) * 0.1;
   dump.Triggers = d.getUint16(14, true);
   dump.TimeMS = d.getUint16(16, true);
   dump.TimeDH = d.getUint16(18, true);
   dump.TimeYM = d.getUint16(20, true);
+
   dump.Faults = d.getUint16(22, true);
   index += step;
   s = dump.T1000Hz.toString() + ";" +
@@ -50,20 +50,41 @@ while (index < FileSize) {
       dump.UgenAB.toString() + ";" +
       dump.UgenBC.toString() + ";" +
       dump.UgenCA.toString() + ";" +
-      dump.Iexc.toString() + ";" +
-      dump.Uexc.toString() + ";" +
+      dump.Iexc.toString().replace(".",",") + ";" +//в Excel нужна запятая
+      dump.Uexc.toString().replace(".",",") + ";" +
       dump.Triggers.toString() + ";" +
-      //Time
       dump.Faults.toString() + ";" +
+      //собрать дату из TimeMS TimeDH TimeYM и T1000Hz
+      getDumpTime(dump.TimeMS, dump.TimeDH, dump.TimeYM, dump.T1000Hz) +
       "\n";
   res += s;
 }
 
-//const Areas: Array<TFlashSegmen> = [...getResourses(Settings.resources || undefined)];
-//write the Array binary data to res.bin file
 fs.writeFileSync("./res.csv", res);
 
 console.log('Complete DUMB2CSV');
+
+function getDumpTime (TimeMS: number, TimeDH: number, TimeYM: number, T1000Hz : number): string {
+  let res: string = '';
+  let year: number = 2000 + (TimeYM >> 8); 
+  let month: number = (TimeYM & 0xFF);
+  let day: number = (TimeDH >> 8); 
+  let hours: number = (TimeDH & 0xFF);
+  let minutes: number = (TimeMS >> 8);
+  let seconds: number = (TimeMS & 0xFF);
+  let ms: number = T1000Hz;
+  let date = new Date(year, month, day, hours, minutes, seconds, ms);
+  //TODO формат данных дата-время с милисекундами м/д/гггг ч:мм:сс.000
+  res = date.getMonth().toString() + '/' +
+        date.getDate().toString() + '/' +
+        date.getFullYear().toString() + '/' +
+        ' ' +
+        date.getHours() + ':' +
+        date.getMinutes() + ':' +
+        date.getSeconds() + '.' +
+        date.getMilliseconds();
+  return res;
+}
 
 /*
 файл состоит из таких записей
